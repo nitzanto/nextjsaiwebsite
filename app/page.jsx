076716749888ecb995e4dad1423acd20fcfc4f1d
nitzanto <nitzan16001@gmail.com>
2023-09-06 21:14:40 +0300

    * Added a home page

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+    return (
+        <section className="w-full flex-center flex-col text-center">
+            <h1 className="head_text">Discover & Share
+                <br className="max-md:hidden"/>
+                <span className="orange_gradient">AI-Powered Prompts</span>
+            </h1>
+            <p className="desc">
+                Promptopia is an open-source AI prompting tool
+            </p>
+        </section>
+);
+};
+
+export default Home;
